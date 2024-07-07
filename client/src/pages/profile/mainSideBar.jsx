72c7475cc24abf72ce1b3ce904393617d068ddf4
ruth-katzir <rutkatzir@gmail.com>
2024-07-07 22:53:05 +0300

    change text in mainSideBar

--- client/src/pages/profile/mainSideBar.jsx
+++ client/src/pages/profile/mainSideBar.jsx
@@ -19,31 +19,15 @@ const Main = (props) => {
     return (
         <>
             <StyledTypography paragraph>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                sapien faucibus et molestie ac.
+                Dear user,
+                Welcome to your personal fitness zone! We’re thrilled to have you here at our state-of-the-art gym. This space is designed to support you in achieving your health and fitness goals, where your progress is honored, and your potential is limitless.
+                Here, you can access your personal fitness data, including your past performances and milestones. This data is a testament to your hard work and determination. But it’s not just about looking back; it's about moving forward. You can review personalized recommendations to propel you towards your next goal.
             </StyledTypography>
             <StyledTypography paragraph>
-                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                posuere sollicitudin aliquam ultrices sagittis orci a.
+                Think of this space as your command center, where you can visualize your progress and make informed decisions about your training. Our system, equipped with cutting-edge AI, provides dynamic workout plans and dietary suggestions tailored just for you.
+                Explore the personalized coaching videos and take advantage of the tools and data to set new goals and map out your path forward. Celebrate your victories, no matter how small, and let this space be your sanctuary and launchpad for success.
+                We believe in you and are here to support you every step of the way. Welcome to the future of your fitness journey, where your potential knows no bounds.
+                Thank you, and let’s make today the start of something amazing!
             </StyledTypography>
         </>
     );
